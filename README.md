b4f23112aaf316287221669c0afd0c61b6fac6cb
Tom Tran <banhaclong20@gmail.com>
2019-02-10 17:14:55 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Next.js react redux starter
+
+An boilerplate for Next.js starter kit with Express, Redux, SASS, and Jest.
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <p>Welcome to Next.js react redux starter</p>
+    <Link href="/about">About Us</Link>
+  </div>
+);
+
+export default Index;
